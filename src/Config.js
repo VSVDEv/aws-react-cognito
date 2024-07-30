5a97390ea630a3f705800f5d8bd53098680cfc3a
VSVDev <vsvdev@example.com>
2024-07-30 17:55:04 +0000

    first

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,7 @@
 //Variables to replace
-const poolId = '[UserPoolId]'
-const clientId = '[AppClientId]'
-const api = '[ApiURL]'
+const poolId = 'us-east-1_tpYWGynnV'
+const clientId = '5529qah5gf6uk0hpesous690og'
+const api = 'https://a88xfey8ng.execute-api.us-east-1.amazonaws.com/Prod'
 
 const Config = {
     UserPoolId: poolId,
